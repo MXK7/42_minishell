5a1ffa5a67789ebe15adb2c99c97f74238f594b6
Mehdi Poussier <mpoussie@c2r6p5.42lehavre.fr>
2023-12-21 09:39:27 +0100

    finish echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,7 +6,7 @@
 /*   By: mpoussie <mpoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 01:39:31 by mpoussie          #+#    #+#             */
-/*   Updated: 2023/12/18 20:23:12 by mpoussie         ###   ########.fr       */
+/*   Updated: 2023/12/21 09:38:08 by mpoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,20 @@
 void	_builtin_echo(t_settings *settings, t_global *global)
 {
 	(void)settings;
-	printf("%s\n", global->token);
+	int i;
+	bool param;
+
+	i = 3;
+	param = false;
+	while (global->input[i])
+	{
+		if (global->input[i] == '-' && global->input[i + 1] == 'n')
+			param = true;
+		else
+			ft_printf("%s\n", global->input);
+		i++;
+	}
+	if (!param)
+		ft_printf("\n");
 	return ;
 }
