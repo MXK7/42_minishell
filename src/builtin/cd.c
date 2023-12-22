2d94e8498cc018352409fbb1e4b79a9c94162481
Mehdi Poussier <mpoussie@c1r7p4.42lehavre.fr>
2023-12-22 14:23:00 +0100

    start cd merge start parse

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: mpoussie <mpoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 10:18:31 by mpoussie          #+#    #+#             */
-/*   Updated: 2023/12/21 10:44:12 by mpoussie         ###   ########.fr       */
+/*   Updated: 2023/12/22 14:21:58 by mpoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,19 @@
 
 void	_builtin_cd(t_global *global)
 {
-    
-    // chdir
+    int fd;
+    printf("%s\n%s", global->args_path[0], global->args_path[1])
+    // opendir: verifie si le chemin d'acces existe
+    if (opendir(global->args_path[1]) != NULL)
+    {
+        fd = open(global->args_path[1], O_RDONLY);
+        if (fd != -1)
+        {
+            printf("GOOD");
+            // TODO: 
+        }
+    }
+    // chdir: se conduit vers le chemin d'acces
+
+    // TODO update_env: PWD change vers le new chemin d'acces
 }
