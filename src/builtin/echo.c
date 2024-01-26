4f8485f1e45624dafe09cae04433977a6a8d3a00
MXK7 <medixom@gmail.com>
2024-01-26 15:40:09 +0100

    fix echo

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,21 +6,21 @@
 /*   By: mpoussie <mpoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 01:39:31 by mpoussie          #+#    #+#             */
-/*   Updated: 2024/01/25 17:12:17 by mpoussie         ###   ########.fr       */
+/*   Updated: 2024/01/26 15:38:27 by mpoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// TODO handler echo flag
-
 int	_builtin_echo(t_global *global)
 {
-	int i = 1;
-	bool new_line = false;
+	int		i;
+	bool	new_line;
 
+	i = 1;
+	new_line = false;
 	while (global->command_list->str[i] != NULL
-		&& global->command_list->str[i][0] == '-'
+		&& global->command_list->str[1][0] == '-' && global->command_list->str[1][1] == 'n'
 		&& ft_strchr(global->command_list->str[i], 'n'))
 	{
 		new_line = true;
