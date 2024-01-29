c86fee15241fff397dd6c9d4c41d7845efa74f59
delin0 <lhp6.arazzok@gmail.com>
2024-01-29 20:14:18 +0100

    fix expander

--- src/expanding/expander.c
+++ src/expanding/expander.c
@@ -6,7 +6,7 @@
 /*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 12:51:31 by arazzok           #+#    #+#             */
-/*   Updated: 2024/01/28 01:39:31 by arazzok          ###   ########.fr       */
+/*   Updated: 2024/01/29 20:11:40 by arazzok          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ static char	*find_env_var(char *start, char **end)
 {
 	start = ft_strchr(start, '$');
 	if (start)
-    {
-        *end = start + 1;
-        while (ft_isalnum(**end) || **end == '_')
-            (*end)++;
-        return (ft_strndup(start + 1, *end - start - 1));
-    }
-    return (NULL);
+	{
+		*end = start + 1;
+		while (ft_isalnum(**end) || **end == '_')
+			(*end)++;
+		return (ft_strndup(start + 1, *end - start - 1));
+	}
+	return (NULL);
 }
 
 static char	*get_env_var_value(char *name)
@@ -39,54 +39,69 @@ static char	*replace_env_var(char *result, char *start, char *end, char *value)
 {
 	char	*new;
 	size_t	len;
+	size_t	start_offset;
+	char	*end_copy;
 
-	len = ft_strlen(result) - (end - start + 1) + ft_strlen(value);
+	end_copy = ft_strdup(end);
+	start_offset = start - result;
+	len = start_offset + ft_strlen(value) + ft_strlen(end_copy);
 	new = malloc(sizeof(char) * (len + 1));
 	if (!new)
+	{
+		free(end_copy);
 		return (NULL);
-	ft_strncpy(new, result, start - result);
-	new[start - result] = '\0';
-	ft_strcat(new, value);
-	ft_strcat(new, end + 1);
+	}
+	ft_strncpy(new, result, start_offset);
+	new[start_offset] = '\0';
+	ft_strcat(new + start_offset, value);
+	ft_strcat(new + start_offset + ft_strlen(value), end_copy);
+	new[len] = '\0';
 	free(result);
+	free(end_copy);
 	return (new);
 }
 
-static void process_expand(char **start, char **result, bool is_quote, char **end)
+static void	process_expand(char **start, char **result, bool is_quote,
+		char **end)
 {
-    char    *name;
-    char    *value;
+	char	*name;
+	char	*value;
 
-    if (**start == '$' && !is_quote)
-    {
-        name = find_env_var(*start, end);
-        if (name)
-        {
-            value = get_env_var_value(name);
-            *result = replace_env_var(*result, *start, *end, value);
-            *start = *result + (*start - *result) + ft_strlen(value);
-            free(name);
-        }
-    }
+	if (**start == '$' && !is_quote)
+	{
+		name = find_env_var(*start, end);
+		if (name)
+		{
+			value = get_env_var_value(name);
+			*result = replace_env_var(*result, *start, *end, value);
+			*start = *result + (*start - *result) + ft_strlen(value);
+			free(name);
+		}
+	}
 }
 
 char	*expand_env_var(char *command)
-{    
-    char	*result;
-    char	*start;
-    char	*end;
-    bool	is_quote;
+{
+	char	*result;
+	char	*start;
+	char	*end;
+	bool	is_quote;
+	size_t	offset;
 
-    is_quote = false;
-    result = ft_strdup(command);
-    start = result;
-    while (*start)
-    {
-        if (*start == '\"')
-            is_quote = !is_quote;
-        else
-            process_expand(&start, &result, is_quote, &end);
-        start++;
-    }
-    return (result);
+	is_quote = false;
+	result = ft_strdup(command);
+	start = result;
+	while (*start)
+	{
+		if (*start == '\"')
+			is_quote = !is_quote;
+		else
+		{
+			offset = start - result;
+			process_expand(&start, &result, is_quote, &end);
+			start = result + offset;
+		}
+		start++;
+	}
+	return (result);
 }
